5b1f4ca9f951b60739c99fb33f1996196205a7c1
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-28 23:24:36 -0500

    Menu appears and disappears with js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
-let buttonMenu = document.getElementById("button_menu")
 
-function upMenu(){
-    let menu = document.querySelector("#menu_up")
-    menu.setAttribute("style", "display:flex")
+function toShowMenu(){
+    let downMenuContainer = document.querySelector('#downMenuContainer')
+    downMenuContainer.classList.remove('container__menu-hidden')
+    downMenuContainer.classList.add('container__menu-content')
 }
 
-function downMenu(){
-    let menu = document.querySelector("#menu_up")
-    menu.setAttribute("style", "display:none")
+function hiddenMenu(){
+    let downMenuContainer = document.querySelector('#downMenuContainer')
+    downMenuContainer.classList.add('container__menu-hidden')
+    downMenuContainer.classList.remove('container__menu-content')
 }
