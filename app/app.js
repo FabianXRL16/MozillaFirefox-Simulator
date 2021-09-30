52a6785f712cd3ecc317fc155481ede6f01996f1
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-29 20:07:31 -0500

    Button of close and toShow Menu fuctional

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@ function toShowMenu(){
     let downMenuContainer = document.querySelector('#downMenuContainer')
     downMenuContainer.classList.remove('container__menu-hidden')
     downMenuContainer.classList.add('container__menu-content')
+    downMenuContainer.classList.add('animationAdd')
 }
 
 function hiddenMenu(){
     let downMenuContainer = document.querySelector('#downMenuContainer')
-    downMenuContainer.classList.add('container__menu-hidden')
     downMenuContainer.classList.remove('container__menu-content')
+    downMenuContainer.classList.add('container__menu-hidden')
+
 }
